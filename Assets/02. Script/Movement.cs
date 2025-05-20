1080e094763f6c74d1609664d9ced3b827c7dca9
Thongmin <wgshdm0627@gmail.com>
2025-05-20 10:58:18 +0900

    [ADD] Unity Preject
    25.05.20
    Unity Study 01 Default files.
    
    -- Assets
    -- Packages
    -- ProjectSettings

---
+++ Assets/02. Script/Movement.cs
@@ -0,0 +1,35 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+    }
+}
